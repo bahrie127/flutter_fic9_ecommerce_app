9fd532c4bf51fda441951642543a6d367cb047ce
Saiful Bahri <bahrie172@gmail.com>
2023-11-04 11:30:01 +0700

    Update variables.dart

--- lib/common/constants/variables.dart
+++ lib/common/constants/variables.dart
@@ -1,5 +1,5 @@
 class Variables {
   static const String baseUrl = 'https://fic9.flutterdev.my.id';
 
-  static const String rajaOngkirKey = 'a9da3c4359fafde97f03ee2be60147b2';
+  static const String rajaOngkirKey = 'APIKEYKAMU59fafde97f03ee2be60147b2';
 }
