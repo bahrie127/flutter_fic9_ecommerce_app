d26e34f6c895e084692dba7d70310f5a5392d289
bahri <saiful.bahri.tl@gmail.com>
2023-11-05 22:28:07 +0700

    rename

--- lib/common/constants/variables.dart
+++ lib/common/constants/variables.dart
@@ -1,5 +1,5 @@
 class Variables {
   static const String baseUrl = 'https://fic9.flutterdev.my.id';
 
-  static const String rajaOngkirKey = 'a9da3c4359fafde97f03ee2be60147b2';
+  static const String rajaOngkirKey = 'APIKEYKAMUfafde97f03ee2be60147b2';
 }
